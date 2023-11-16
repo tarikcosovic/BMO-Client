67661484ffa33f6bea039534a35db7857a85e235
tarikcosovic <tarikcosovic@gmail.com>
2023-11-17 00:38:36 +0100

    transfering api from azure to render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://bmo-api.dsgmf8fyafevgtcz.northeurope.azurecontainer.io/api/', //local - http://localhost:8080/api/',
+  apiUrl: 'https://bmo-api.onrender.com/api/', //local - http://localhost:8080/api/',
 };
